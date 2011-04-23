d5424a4aa12a44c6b2e5ebec1036d9aaffe52cf4
Akira Matsuda <ronnie@dio.jp>
2011-04-23 11:55:28 +0900

    tie them up!

--- lib/merbish.rb
+++ lib/merbish.rb
@@ -1,3 +1,11 @@
+require File.join(File.dirname(__FILE__), 'merbish/abstract_controller')
+
 module Merbish
-  # Your code goes here...
+  class Railtie < ::Rails::Railtie
+    if config.respond_to? :app_generators
+      config.app_generators.scaffold_controller = :merbish_scaffold_controller
+    else
+      config.generators.scaffold_controller = :merbish_scaffold_controller
+    end
+  end
 end

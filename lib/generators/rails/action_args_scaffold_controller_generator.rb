2ce3991a6be66ad2b22f62c07404ab5d0e79fd86
Akira Matsuda <ronnie@dio.jp>
2012-11-26 21:24:32 +0900

    the Scaffold generator now generates `permits` declaration

--- lib/generators/rails/action_args_scaffold_controller_generator.rb
+++ lib/generators/rails/action_args_scaffold_controller_generator.rb
@@ -5,6 +5,7 @@ require 'rails/generators/rails/scaffold_controller/scaffold_controller_generato
 module Rails
   module Generators
     class ActionArgsScaffoldControllerGenerator < ::Rails::Generators::ScaffoldControllerGenerator
+      argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
       source_root File.expand_path('../templates', __FILE__)
     end
   end
--- lib/generators/rails/templates/controller.rb
+++ lib/generators/rails/templates/controller.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 
 class <%= controller_class_name %>Controller < ApplicationController
+<% if defined? ActionController::StrongParameters -%>
+  permits <%= attributes.map {|a| ":#{a.name}" }.join(', ') %>
+
+<% end -%>
   # GET <%= route_url %>
   def index
     @<%= plural_table_name %> = <%= orm_class.all(class_name) %>

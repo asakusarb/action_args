b3683c389d5bb8b85162d48bc5691d24d3f48233
Akira Matsuda <ronnie@dio.jp>
2012-12-02 05:58:59 +0900

    use built-in controller instance rather than Controller.new

--- lib/generators/action_args/rspec/scaffold/templates/action_args_controller_spec.rb
+++ lib/generators/action_args/rspec/scaffold/templates/action_args_controller_spec.rb
@@ -7,19 +7,15 @@ describe <%= controller_class_name %>Controller do
   def valid_attributes
     {}
   end
-  
-  before do
-    @controller = <%= controller_class_name %>Controller.new
-  end
 
 <% unless options[:singleton] -%>
   describe 'GET index' do
     before do
       @<%= file_name %> = <%= class_name %>.create! valid_attributes
-      @controller.index
+      controller.index
     end
     describe 'assigns all <%= table_name.pluralize %> as @<%= table_name.pluralize %>' do
-      subject { @controller.instance_variable_get('@<%= table_name %>') }
+      subject { controller.instance_variable_get('@<%= table_name %>') }
       it { should eq([@<%= file_name %>]) }
     end
   end
@@ -28,20 +24,20 @@ describe <%= controller_class_name %>Controller do
   describe 'GET show' do
     before do
       @<%= file_name %> = <%= class_name %>.create! valid_attributes
-      @controller.show(@<%= file_name %>.to_param)
+      controller.show(@<%= file_name %>.to_param)
     end
     describe 'assigns the requested <%= ns_file_name %> as @<%= ns_file_name %>' do
-      subject { @controller.instance_variable_get('@<%= ns_file_name %>') }
+      subject { controller.instance_variable_get('@<%= ns_file_name %>') }
       it { should eq(@<%= file_name %>) }
     end
   end
 
   describe 'GET new' do
     before do
-      @controller.new
+      controller.new
     end
     describe 'assigns a new <%= ns_file_name %> as @<%= ns_file_name %>' do
-      subject { @controller.instance_variable_get('@<%= ns_file_name %>') }
+      subject { controller.instance_variable_get('@<%= ns_file_name %>') }
       it { should be_a_new(<%= class_name %>) }
     end
   end
@@ -49,10 +45,10 @@ describe <%= controller_class_name %>Controller do
   describe 'GET edit' do
     before do
       @<%= file_name %> = <%= class_name %>.create! valid_attributes
-      @controller.edit(@<%= file_name %>.to_param)
+      controller.edit(@<%= file_name %>.to_param)
     end
     describe 'assigns the requested <%= ns_file_name %> as @<%= ns_file_name %>' do
-      subject { @controller.instance_variable_get('@<%= ns_file_name %>') }
+      subject { controller.instance_variable_get('@<%= ns_file_name %>') }
       it { should eq(@<%= file_name %>) }
     end
   end
@@ -60,19 +56,19 @@ describe <%= controller_class_name %>Controller do
   describe 'POST create' do
     context 'with valid params' do
       before do
-        @controller.should_receive(:redirect_to) {|u| u.should eq(<%= class_name %>.last) }
+        controller.should_receive(:redirect_to) {|u| u.should eq(<%= class_name %>.last) }
       end
       describe 'creates a new <%= class_name %>' do
         it { expect {
-          @controller.create(valid_attributes)
+          controller.create(valid_attributes)
         }.to change(<%= class_name %>, :count).by(1) }
       end
 
       describe 'assigns a newly created <%= ns_file_name %> as @<%= ns_file_name %> and redirects to the created <%= ns_file_name %>' do
         before do
-          @controller.create(valid_attributes)
+          controller.create(valid_attributes)
         end
-        subject { @controller.instance_variable_get('@<%= ns_file_name %>') }
+        subject { controller.instance_variable_get('@<%= ns_file_name %>') }
         it { should be_a(<%= class_name %>) }
         it { should be_persisted }
       end
@@ -82,10 +78,10 @@ describe <%= controller_class_name %>Controller do
       describe "assigns a newly created but unsaved <%= ns_file_name %> as @<%= ns_file_name %>, and re-renders the 'new' template" do
         before do
           <%= class_name %>.any_instance.stub(:save) { false }
-          @controller.should_receive(:render).with(:action => 'new')
-          @controller.create({})
+          controller.should_receive(:render).with(:action => 'new')
+          controller.create({})
         end
-        subject { @controller.instance_variable_get('@<%= ns_file_name %>') }
+        subject { controller.instance_variable_get('@<%= ns_file_name %>') }
         it { should be_a_new(<%= class_name %>) }
       end
     end
@@ -96,10 +92,10 @@ describe <%= controller_class_name %>Controller do
       describe 'updates the requested <%= ns_file_name %>, assigns the requested <%= ns_file_name %> as @<%= ns_file_name %>, and redirects to the <%= ns_file_name %>' do
         before do
           @<%= file_name %> = <%= class_name %>.create! valid_attributes
-          @controller.should_receive(:redirect_to).with(@<%= file_name %>, anything)
-          @controller.update(@<%= file_name %>.to_param, valid_attributes)
+          controller.should_receive(:redirect_to).with(@<%= file_name %>, anything)
+          controller.update(@<%= file_name %>.to_param, valid_attributes)
         end
-        subject { @controller.instance_variable_get('@<%= ns_file_name %>') }
+        subject { controller.instance_variable_get('@<%= ns_file_name %>') }
         it { should eq(@<%= file_name %>) }
       end
     end
@@ -110,10 +106,10 @@ describe <%= controller_class_name %>Controller do
           @<%= file_name %> = <%= class_name %>.create! valid_attributes
           # Trigger the behavior that occurs when invalid params are submitted
           <%= class_name %>.any_instance.stub(:save) { false }
-          @controller.should_receive(:render).with(:action => 'edit')
-          @controller.update(@<%= file_name %>.to_param, {})
+          controller.should_receive(:render).with(:action => 'edit')
+          controller.update(@<%= file_name %>.to_param, {})
         end
-        subject { @controller.instance_variable_get('@<%= ns_file_name %>') }
+        subject { controller.instance_variable_get('@<%= ns_file_name %>') }
         it { should eq(@<%= file_name %>) }
       end
     end
@@ -122,12 +118,12 @@ describe <%= controller_class_name %>Controller do
   describe 'DELETE destroy' do
     before do
       @<%= file_name %> = <%= class_name %>.create! valid_attributes
-      @controller.stub(:<%= index_helper %>_url) { '/<%= index_helper %>' }
-      @controller.should_receive(:redirect_to).with('/<%= index_helper %>')
+      controller.stub(:<%= index_helper %>_url) { '/<%= index_helper %>' }
+      controller.should_receive(:redirect_to).with('/<%= index_helper %>')
     end
     it 'destroys the requested <%= ns_file_name %>, and redirects to the <%= table_name %> list' do
       expect {
-        @controller.destroy(@<%= file_name %>.to_param)
+        controller.destroy(@<%= file_name %>.to_param)
       }.to change(<%= class_name %>, :count).by(-1)
     end
   end

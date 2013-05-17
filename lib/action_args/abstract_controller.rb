401bbf1fa411439f9e53ae3d27f0e0704a58d9b9
Akira Matsuda <ronnie@dio.jp>
2013-05-17 10:42:03 +0900

    Don't nilify :opt arguments that are preceeding a :req argument
    e.g.
    def index(opt1 = 1, req1) end + params: {req1: 'aho'}
    will pass in the default value 1 for "opt1" parameter

--- lib/action_args/abstract_controller.rb
+++ lib/action_args/abstract_controller.rb
@@ -6,20 +6,26 @@ module AbstractController
 
         target_model_name = self.class.name.sub(/.+::/, '').sub(/Controller$/, '').singularize.underscore.to_sym
         permitted_attributes = self.class.instance_variable_get '@permitted_attributes'
-        values, kwargs = [], {}
-        method(method_name).parameters.reverse_each.drop_while {|type,key| type == :block || type == :opt && ! params.has_key?(key) }.reverse_each do |type, key|
-          params.require key if type == :req
-          val = if (key == target_model_name) && permitted_attributes
-            params[key].try :permit, *permitted_attributes
-          else
-            params[key]
+        kwargs = {}
+        parameter_names = method(method_name).parameters.map(&:last)
+        method(method_name).parameters.reverse_each do |type, key|
+          if (key == target_model_name) && permitted_attributes
+            params[key] = params.require(key).try :permit, *permitted_attributes
           end
-          if type == :key
-            kwargs[key] = val if params.has_key? key
-          else
-            values << val
+
+          case type
+          when :req
+            params.require(key)
+            next
+          when :key
+            kwargs[key] = params[key] if params.has_key? key
+          when :opt
+            break if params.has_key? key
           end
+          parameter_names.delete key
         end
+
+        values = parameter_names.map {|k| params[k]}
         values << kwargs if kwargs.any?
         send method_name, *values
       end
@@ -44,14 +50,21 @@ module AbstractController
       def send_action(method_name, *args)
         return send method_name, *args unless args.empty?
 
-        values, kwargs = [], {}
-        method(method_name).parameters.reverse_each.drop_while {|type,key| type == :block || type == :opt && ! params.has_key?(key) }.reverse_each do |type, key|
-          if type == :key
+        kwargs = {}
+        parameter_names = method(method_name).parameters.map(&:last)
+        method(method_name).parameters.reverse_each do |type, key|
+          case type
+          when :req
+            next
+          when :key
             kwargs[key] = params[key] if params.has_key? key
-          else
-            values << params[key]
+          when :opt
+            break if params.has_key? key
           end
+          parameter_names.delete key
         end
+
+        values = parameter_names.map {|k| params[k]}
         values << kwargs if kwargs.any?
         send method_name, *values
       end

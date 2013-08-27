c051cc73417cf24d8cc1b94ab1a4492a5835538f
Akira Matsuda <ronnie@dio.jp>
2013-08-27 14:19:58 +0900

    "DEPRECATION: `expect { }.not_to raise_error(SpecificErrorClass)` is deprecated. Use `expect { }.not_to raise_error()` instead."

--- spec/mailers/action_mailer_spec.rb
+++ spec/mailers/action_mailer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UserMailer do
   describe '#send_email_without_args' do
     it "should not raise NameError: undefined local variable or method `params' for ..." do
-      expect{ UserMailer.send_email_without_args }.to_not raise_error(NameError)
+      expect{ UserMailer.send_email_without_args }.to_not raise_error
     end
   end
 end

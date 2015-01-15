b85576fd70f580ed64ad6454fa7b57a55fabd9ac
Akira Matsuda <ronnie@dio.jp>
2015-01-15 23:45:26 +0900

    Safer monkey-patching

--- lib/action_args/action_controller.rb
+++ lib/action_args/action_controller.rb
@@ -1,19 +1,19 @@
-module ActionController
-  class Base
-    class << self
-      def set_callback(name, *filter_list, &block)
-        if Symbol === filter_list[1]
-          sym = filter_list[1]
-          filter_list[1] = Proc.new {
-            meth = method(sym)
-            method_parameters = meth.parameters
-            ActionArgs::ParamsHandler.strengthen_params!(self.class, method_parameters, params)
-            values = ActionArgs::ParamsHandler.extract_method_arguments_from_params method_parameters, params
-            send sym, *values
-          }
-        end
-        super
+module ActionArgs
+  module Callbacks
+    def set_callback(name, *filter_list, &block)
+      if Symbol === filter_list[1]
+        sym = filter_list[1]
+        filter_list[1] = Proc.new {
+          meth = method(sym)
+          method_parameters = meth.parameters
+          ActionArgs::ParamsHandler.strengthen_params!(self.class, method_parameters, params)
+          values = ActionArgs::ParamsHandler.extract_method_arguments_from_params method_parameters, params
+          send sym, *values
+        }
       end
+      super
     end
   end
 end
+
+ActionController::Base.singleton_class.send :prepend, ActionArgs::Callbacks

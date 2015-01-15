8824357028ae2463aaa6bc242a30056b8a832c23
Akira Matsuda <ronnie@dio.jp>
2015-01-16 07:56:20 +0900

    :bomb: :skull: Ruby 1.9 :hocho: :fire:

--- lib/action_args/action_controller.rb
+++ lib/action_args/action_controller.rb
@@ -1,19 +1,21 @@
-module ActionArgs
-  module Callbacks
-    def set_callback(name, *filter_list, &block)
-      if Symbol === filter_list[1]
-        sym = filter_list[1]
-        filter_list[1] = Proc.new {
-          meth = method(sym)
-          method_parameters = meth.parameters
-          ActionArgs::ParamsHandler.strengthen_params!(self.class, method_parameters, params)
-          values = ActionArgs::ParamsHandler.extract_method_arguments_from_params method_parameters, params
-          send sym, *values
-        }
+module ActionController
+  class Base
+    class << self
+      def set_callback_with_symbol_to_proc(name, *filter_list, &block)
+        if Symbol === filter_list[1]
+          sym = filter_list[1]
+          filter_list[1] = Proc.new {
+            meth = method(sym)
+            method_parameters = meth.parameters
+            ActionArgs::ParamsHandler.strengthen_params!(self.class, method_parameters, params)
+            values = ActionArgs::ParamsHandler.extract_method_arguments_from_params method_parameters, params
+            send sym, *values
+          }
+        end
+        set_callback_without_symbol_to_proc(name, *filter_list, &block)
       end
-      super
+
+      alias_method_chain :set_callback, :symbol_to_proc
     end
   end
 end
-
-ActionController::Base.singleton_class.send :prepend, ActionArgs::Callbacks

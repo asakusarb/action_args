29e0b9fba176453d2218a017e33bd7b6f84bc3e9
Akira Matsuda <ronnie@dio.jp>
2015-01-16 11:14:49 +0900

    :memo:

--- lib/action_args/action_controller.rb
+++ lib/action_args/action_controller.rb
@@ -1,6 +1,15 @@
 module ActionController
   class Base
     class << self
+      # Redefining AS::Callback's `set_callback` to convert Symbol callbacks
+      # into Procs that call given methods with method parameters taken from `params`.
+      #
+      # For example, if
+      #   `before_action :set_user`
+      # was defined in a controller, set_callback method receives parameters like this:
+      #   {:name=>:process_action, :filter_list=>[:before, :set_user, {}]}
+      # then this monkey-patch would convert `:set_user` to a Proc that calls `set_user`
+      # method with proper params.
       def set_callback_with_symbol_to_proc(name, *filter_list, &block)
         if Symbol === filter_list[1]
           sym = filter_list[1]

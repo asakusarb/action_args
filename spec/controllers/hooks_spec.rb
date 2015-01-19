57dccc5b31cb3a2be4b23dcb5a9213d785099645
Akira Matsuda <ronnie@dio.jp>
2015-01-19 21:40:28 +0900

    I said, lets's make sure that around_action is working

--- spec/controllers/hooks_spec.rb
+++ spec/controllers/hooks_spec.rb
@@ -1,24 +1,31 @@
 require 'spec_helper'
 
-describe BooksController, 'before_action hooks' do
+describe BooksController, 'action hooks' do
   before do
     Book.delete_all
     @book = Book.create! title: 'Head First ActionArgs'
     get :show, id: @book.id
   end
 
-  context 'via Symbol' do
-    subject { assigns :book }
-    it { should == @book }
+  describe 'before_action' do
+    context 'via Symbol' do
+      subject { assigns :book }
+      it { should == @book }
+    end
+
+    context 'via String' do
+      subject { assigns :string_filter_executed }
+      it { should be_true }
+    end
+
+    context 'via Proc' do
+      subject { assigns :proc_filter_executed }
+      it { should be_true }
+    end
   end
 
-  context 'via String' do
-    subject { assigns :string_filter_executed }
-    it { should be_true }
-  end
-
-  context 'via Proc' do
-    subject { assigns :proc_filter_executed }
-    it { should be_true }
+  describe 'around_action' do
+    subject { assigns :elapsed_time }
+    it { should be }
   end
 end

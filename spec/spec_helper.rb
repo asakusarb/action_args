137a79ad009b246f3a8c9c5a06df9a5c0801d3f2
Akira Matsuda <ronnie@dio.jp>
2015-08-24 03:55:40 +0900

    Enable rails-controller-testing Modules in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,13 @@ require 'action_controller/railtie'
 require 'action_args'
 require 'fake_app'
 require 'rspec/rails'
+require 'rails-controller-testing'
 
 RSpec.configure do |config|
+  config.include Rails::Controller::Testing::TestProcess
+  config.include Rails::Controller::Testing::TemplateAssertions
+  config.include Rails::Controller::Testing::Integration
+
   config.before :all do
     CreateAllTables.up unless ActiveRecord::Base.connection.table_exists? 'authors'
   end

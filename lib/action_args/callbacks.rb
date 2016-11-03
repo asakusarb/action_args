b31b1812b9ebe2514262802b925459c8ae1b39a1
Akira Matsuda <ronnie@dio.jp>
2016-11-03 11:58:44 +0900

    :arrow_left: :arrow_left:

--- lib/action_args/callbacks.rb
+++ lib/action_args/callbacks.rb
@@ -4,26 +4,26 @@ using ActionArgs::ParamsHandler
 module ActionArgs
   module ActiveSupport
     module CallbackParameterizer
-        # Extending AS::Callbacks::Callback's `make_lambda` not just to call specified
-        # method but to call the method with method parameters taken from `params`.
-        # This would happen only when
-        # * the filter was defined in Symbol form
-        # * the target object is_a ActionController object
-        def make_lambda(filter)
-          if Symbol === filter
-            lambda do |target, _, &blk|
-              if ActionController::Base === target
-                target.strengthen_params! filter
-                values = target.extract_method_arguments_from_params filter
-                target.send filter, *values, &blk
-              else
-                target.send filter, &blk
-              end
+      # Extending AS::Callbacks::Callback's `make_lambda` not just to call specified
+      # method but to call the method with method parameters taken from `params`.
+      # This would happen only when
+      # * the filter was defined in Symbol form
+      # * the target object is_a ActionController object
+      def make_lambda(filter)
+        if Symbol === filter
+          lambda do |target, _, &blk|
+            if ActionController::Base === target
+              target.strengthen_params! filter
+              values = target.extract_method_arguments_from_params filter
+              target.send filter, *values, &blk
+            else
+              target.send filter, &blk
             end
-          else
-            super
           end
+        else
+          super
         end
+      end
     end
   end
 end
